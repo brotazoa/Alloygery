cd04356cd3fbaf05ec4dca445a0bf77153910068
brotazoa <brotazoa@brotazoa.com>
2022-01-30 17:23:30 -0500

    Removed built in Alloy Forgery support in favor of support via datapacks Updated ore textures
    To better support datapacks all of Alloygery's config options are queryable via resource conditions. Alloygery provides three load conditions: config_boolean_condition, config_integer_condition, and config_float_condition. Each are documented in ModResourceConditions

--- src/main/java/amorphia/alloygery/registry/ModResourceConditions.java
+++ src/main/java/amorphia/alloygery/registry/ModResourceConditions.java
@@ -9,14 +9,122 @@ import net.minecraft.util.JsonHelper;
 public class ModResourceConditions
 {
 	public static final Identifier CONFIG_BOOLEAN_CONDITION = Alloygery.identifier("config_boolean_condition");
+	public static final Identifier CONFIG_INTEGER_CONDITION = Alloygery.identifier("config_integer_condition");
+	public static final Identifier CONFIG_FLOAT_CONDITION = Alloygery.identifier("config_float_condition");
 
 	public static void register()
 	{
 		ResourceConditions.register(CONFIG_BOOLEAN_CONDITION, ModResourceConditions::configBooleanConditionImpl);
+		ResourceConditions.register(CONFIG_INTEGER_CONDITION, ModResourceConditions::configIntegerConditionImpl);
+		ResourceConditions.register(CONFIG_FLOAT_CONDITION, ModResourceConditions::configFloatConditionImpl);
 	}
 
+	/**
+	 * @param json A {@link JsonObject} representing the resource load condition
+	 * @return Returns the boolean value for the provided config
+	 *
+	 * <h3>Example Usage</h3>
+	 * <pre>{@code
+	 * {
+	 *   ... // normal contents of the resource
+	 *   "fabric:load_conditions": [ // array of condition objects
+	 *     {
+	 *       // the identifier of the condition
+	 *       "condition": "alloygery:config_boolean_condition",
+	 *       // the config to test. Note you can target values in inner classes by specifying the path
+	 *       "config": "copperGear.enable"
+	 *       // this will make a condition where the resource is only loaded if copper gear is enabled in the config
+	 *     }
+	 *   ]
+	 * }
+	 * }</pre>
+	 */
 	public static boolean configBooleanConditionImpl(JsonObject json)
 	{
 		return Alloygery.CONFIG.getBooleanValue(JsonHelper.getString(json, "config"), false);
+	}
+
+	/**
+	 * @param json A {@link JsonObject} representing the resource load condition
+	 * @return Returns true if the config matches the desired value
+	 *
+	 * <h3>Example Usage</h3>
+	 * <pre>{@code
+	 * {
+	 *   ... // normal contents of the resource
+	 *   "fabric:load_conditions": [ // array of condition objects
+	 *     {
+	 *       // the identifier of the condition
+	 *       "condition": "alloygery:config_integer_condition",
+	 *       // the config to test. Note you can target values in inner classes by specifying the path
+	 *       "config": "copperGear.miningLevel",
+	 *       // matches can be one of "equals", "=", "greaterThan", ">", "lessThan", ">", "greaterThanOrEqual", ">=", "lessThanOrEqual", or "<=".
+	 *       "matches": "equals",
+	 *       "value": 1
+	 *       // this will make a condition where the resource is only loaded if copper mining level is equal to 1
+	 *     }
+	 *   ]
+	 * }
+	 * }</pre>
+	 */
+	public static boolean configIntegerConditionImpl(JsonObject json)
+	{
+		String configString = JsonHelper.getString(json, "config");
+		String matchString = JsonHelper.getString(json, "matches");
+		int valueInteger = JsonHelper.getInt(json, "value");
+
+		int configValue = Alloygery.CONFIG.getIntegerValue(configString, 0);
+
+		return switch (matchString)
+		{
+			case "equals", "=" -> configValue == valueInteger;
+			case "greaterThan", ">" -> configValue > valueInteger;
+			case "lessThan", "<" -> configValue < valueInteger;
+			case "greaterThanOrEqual", ">=" -> configValue >= valueInteger;
+			case "lessThanOrEqual", "<=" -> configValue <= valueInteger;
+			default -> false;
+		};
+	}
+
+	/**
+	 * @param json A {@link JsonObject} representing the resource load condition
+	 * @return Returns true if the config matches the desired value
+	 *
+	 * <h3>Example Usage</h3>
+	 * <pre>{@code
+	 * {
+	 *   ... // normal contents of the resource
+	 *   "fabric:load_conditions": [ // array of condition objects
+	 *     {
+	 *       // the identifier of the condition
+	 *       "condition": "alloygery:config_float_condition",
+	 *       // the config to test. Note you can target values in inner classes by specifying the path
+	 *       "config": "copperGear.knockback",
+	 *       // matches can be one of "equals", "=", "greaterThan", ">", "lessThan", ">", "greaterThanOrEqual", ">=", "lessThanOrEqual", or "<=".
+	 *       "matches": "<=",
+	 *       "value": 4.0
+	 *       // this will make a condition where the resource is only loaded if copper gear knockback is less than or equal to 4.0
+	 *     }
+	 *   ]
+	 * }
+	 * }</pre>
+	 */
+	public static boolean configFloatConditionImpl(JsonObject json)
+	{
+		String configString = JsonHelper.getString(json, "config");
+		String matchString = JsonHelper.getString(json, "matches");
+		float valueFloat = JsonHelper.getFloat(json, "value");
+
+		float configValue = Alloygery.CONFIG.getFloatValue(configString, 0.0f);
+
+		return switch (matchString)
+		{
+			case "equals", "=" -> configValue == valueFloat;
+			case "greaterThan", ">" -> configValue > valueFloat;
+			case "lessThan", "<" -> configValue < valueFloat;
+			case "greaterThanOrEqual", ">=" -> configValue >= valueFloat;
+			case "lessThanOrEqual", "<=" -> configValue <= valueFloat;
+			default -> false;
+		};
 	}
 }

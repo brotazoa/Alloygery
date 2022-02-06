f3b3bf23dd44eedfc52df476b8bc33b057169460
brotazoa <brotazoa@brotazoa.com>
2022-02-06 17:35:43 -0500

    Armor recipes updated ru_ru translation contributed by aigiz010 fixes #13

--- src/main/java/amorphia/alloygery/compat/rei/AlloygeryPlugin.java
+++ src/main/java/amorphia/alloygery/compat/rei/AlloygeryPlugin.java
@@ -8,7 +8,6 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 
 public class AlloygeryPlugin implements REIClientPlugin

57c16f6de3bbe8d6969f658e0459ad75368af43d
brotazoa <brotazoa@brotazoa.com>
2022-04-28 13:42:36 -0400

    Smithing Anvil is waterloggable

--- src/main/java/amorphia/alloygery/content/block/SmithingAnvilBlock.java
+++ src/main/java/amorphia/alloygery/content/block/SmithingAnvilBlock.java
@@ -5,12 +5,17 @@ import amorphia.alloygery.registry.ModStatistics;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
@@ -25,11 +30,14 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
-public class SmithingAnvilBlock extends Block
+public class SmithingAnvilBlock extends Block implements Waterloggable
 {
 	private static final Text TITLE = new TranslatableText("container.alloygery.smithing_anvil");
 	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
 	private static final VoxelShape X_BASE;
 	private static final VoxelShape Z_BASE;
 	private static final VoxelShape X_STEP;
@@ -44,12 +52,12 @@ public class SmithingAnvilBlock extends Block
 	public SmithingAnvilBlock(Settings settings)
 	{
 		super(settings);
-		this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+		this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH).with(WATERLOGGED, false));
 	}
 
 	public BlockState getPlacementState(ItemPlacementContext ctx)
 	{
-		return this.getDefaultState().with(FACING, ctx.getPlayerFacing().rotateYClockwise());
+		return this.getDefaultState().with(FACING, ctx.getPlayerFacing().rotateYClockwise()).with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
 	}
 
 	public BlockState rotate(BlockState state, BlockRotation rotation)
@@ -59,7 +67,24 @@ public class SmithingAnvilBlock extends Block
 
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder)
 	{
-		builder.add(FACING);
+		builder.add(FACING, WATERLOGGED);
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state)
+	{
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos,
+			BlockPos neighborPos)
+	{
+		if (state.get(WATERLOGGED))
+		{
+			world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
 	}
 
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)

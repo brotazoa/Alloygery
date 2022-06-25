fd1588d58d6de01f00298fc7cfcbd85d2ae623d4
brotazoa <brotazoa@brotazoa.com>
2022-06-24 22:54:14 -0400

    Patchouli guide book

--- src/main/java/amorphia/alloygery/compat/patchouli/PartSettingsProcessor.java
+++ src/main/java/amorphia/alloygery/compat/patchouli/PartSettingsProcessor.java
@@ -9,7 +9,6 @@ import vazkii.patchouli.api.IVariableProvider;
 public class PartSettingsProcessor implements IComponentProcessor
 {
 	private AlloygeryMaterial.ToolPartSettings partSettings;
-	private boolean multiplier;
 
 	@Override
 	public void setup(IVariableProvider iVariableProvider)
@@ -19,7 +18,6 @@ public class PartSettingsProcessor implements IComponentProcessor
 		AlloygeryMaterial material = AlloygeryMaterial.getById(materialId);
 		this.partSettings = part.equals("head") ? material.tool_base : part.equals("binding") ? material.tool_binding : part.equals("handle") ?
 				material.tool_handle : part.equals("upgrade") ? material.tool_upgrade : material.tool_base;
-		this.multiplier = iVariableProvider.get("modifier").asString().equals("multiplies");
 	}
 
 	@Override
@@ -27,14 +25,55 @@ public class PartSettingsProcessor implements IComponentProcessor
 	{
 		return switch (key)
 		{
-			case "durability" -> multiplier ? IVariable.wrap(partSettings.durability_multiplier) : IVariable.wrap(partSettings.durability);
-			case "mining_level" -> multiplier ? null : IVariable.wrap(partSettings.mining_level);
-			case "mining_speed" -> multiplier ? IVariable.wrap(partSettings.mining_speed_multiplier) : IVariable.wrap(partSettings.mining_speed);
-			case "attack_damage" -> multiplier ? IVariable.wrap(partSettings.attack_damage_multiplier) : IVariable.wrap(partSettings.attack_damage);
-			case "attack_speed" -> multiplier ? IVariable.wrap(partSettings.attack_speed_multiplier) : IVariable.wrap(partSettings.attack_speed);
-			case "enchantability" -> multiplier ? IVariable.wrap(partSettings.enchantability_multiplier) : IVariable.wrap(partSettings.enchantability);
-			case "luck" -> multiplier ? IVariable.wrap(partSettings.luck_multiplier) : IVariable.wrap(partSettings.luck);
+			case "durability" -> IVariable.wrap(partSettings.durability);
+			case "durability_multiplier" -> IVariable.wrap(partSettings.durability_multiplier);
+			case "mining_level" -> IVariable.wrap(partSettings.mining_level);
+			case "mining_speed" -> IVariable.wrap(partSettings.mining_speed);
+			case "mining_speed_multiplier" -> IVariable.wrap(partSettings.mining_speed_multiplier);
+			case "attack_damage" -> IVariable.wrap(partSettings.attack_damage);
+			case "attack_damage_multiplier" -> IVariable.wrap(partSettings.attack_damage_multiplier);
+			case "attack_speed" -> IVariable.wrap(partSettings.attack_speed);
+			case "attack_speed_multiplier" -> IVariable.wrap(partSettings.attack_speed_multiplier);
+			case "enchantability" -> IVariable.wrap(partSettings.enchantability);
+			case "enchantability_multiplier" -> IVariable.wrap(partSettings.enchantability_multiplier);
+			case "luck" -> IVariable.wrap(partSettings.luck);
+			case "luck_multiplier" -> IVariable.wrap(partSettings.luck_multiplier);
+			case "values" -> IVariable.wrap(makeValueString());
 			default -> null;
 		};
+	}
+
+	private String makeValueString()
+	{
+		final String newLine = "$(br)";
+
+		StringBuilder builder = new StringBuilder();
+		if(partSettings.durability != 0)
+			builder.append("Durability: ").append(partSettings.durability).append(newLine);
+		if(partSettings.durability_multiplier != 1.0f)
+			builder.append("Durability Multiplier: ").append(partSettings.durability_multiplier).append(newLine);
+		if(partSettings.mining_level != 0)
+			builder.append("Mining Level: ").append(partSettings.mining_level).append(newLine);
+		if(partSettings.mining_speed != 0.0f)
+			builder.append("Mining Speed: ").append(partSettings.mining_speed).append(newLine);
+		if(partSettings.mining_speed_multiplier != 1.0f)
+			builder.append("Mining Speed Multiplier: ").append(partSettings.mining_speed_multiplier).append(newLine);
+		if(partSettings.attack_damage != 0.0f)
+			builder.append("Attack Damage: ").append(partSettings.attack_damage).append(newLine);
+		if(partSettings.attack_damage_multiplier != 1.0f)
+			builder.append("Attack Damage Multiplier: ").append(partSettings.attack_damage_multiplier).append(newLine);
+		if(partSettings.attack_speed != 0.0f)
+			builder.append("Attack Speed: ").append(partSettings.attack_speed).append(newLine);
+		if(partSettings.attack_speed_multiplier != 1.0f)
+			builder.append("Attack Speed Multiplier: ").append(partSettings.attack_speed_multiplier).append(newLine);
+		if(partSettings.enchantability != 0)
+			builder.append("Enchantability: ").append(partSettings.enchantability).append(newLine);
+		if(partSettings.enchantability_multiplier != 1.0f)
+			builder.append("Enchantability Multiplier: ").append(partSettings.enchantability_multiplier).append(newLine);
+		if(partSettings.luck != 0.0f)
+			builder.append("Luck: ").append(partSettings.luck).append(newLine);
+		if(partSettings.luck_multiplier != 1.0f)
+			builder.append("Luck Multiplier: ").append(partSettings.luck_multiplier);
+		return builder.toString();
 	}
 }

d29e1ec42d8207ac6b6457a82363d822c6ee79f7
brotazoa <brotazoa@brotazoa.com>
2023-02-04 20:51:57 -0500

    Occlude vibration signals

--- src/main/java/amorphia/alloygery/content/armor/registry/ArmorTagRegistry.java
+++ src/main/java/amorphia/alloygery/content/armor/registry/ArmorTagRegistry.java
@@ -8,9 +8,11 @@ import net.minecraft.util.registry.Registry;
 public class ArmorTagRegistry
 {
 	public static TagKey<Item> POWDER_WALK_BOOTS;
+	public static TagKey<Item> OCCLUDES_VIBRATION_SIGNALS_WHEN_WORN;
 
 	public static void init()
 	{
 		POWDER_WALK_BOOTS = TagKey.of(Registry.ITEM_KEY, Alloygery.identifier("armors/can_walk_on_powdered_snow"));
+		OCCLUDES_VIBRATION_SIGNALS_WHEN_WORN = TagKey.of(Registry.ITEM_KEY, Alloygery.identifier("armors/occludes_vibration_signals_when_worn"));
 	}
 }

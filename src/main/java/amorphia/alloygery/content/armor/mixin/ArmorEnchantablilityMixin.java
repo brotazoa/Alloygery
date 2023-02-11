01f0def91ae0efde055fa8a1ad001887b5b37bb9
brotazoa <brotazoa@brotazoa.com>
2023-02-10 21:32:24 -0500

    RC1

--- src/main/java/amorphia/alloygery/content/armor/mixin/ArmorEnchantablilityMixin.java
+++ src/main/java/amorphia/alloygery/content/armor/mixin/ArmorEnchantablilityMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
